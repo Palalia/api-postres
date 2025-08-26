40e1368736f8bdef60da4e0ede36d7882f9c501e
Angel <tonio.palalia@gmail.com>
2025-08-26 17:32:25 -0600

    descarga e implementacion de cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
-
+import routes from "./v1/routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// Middlewares
+app.use(express.json({ limit: "2mb" }));
 
+// Configuración de CORS
+app.use(
+    cors({
+        origin: "*", // puedes poner tu dominio en lugar de "*" si lo quieres restringir
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    })
+);
+
+// Rutas
+app.use("/v1", routes);
+
+// Servidor
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
