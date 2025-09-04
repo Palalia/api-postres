0467c96b75f4b4654ebef7015d65f41ac9520e02
Angel <tonio.palalia@gmail.com>
2025-09-03 18:03:25 -0600

    feat(conceptModel): add Concept model and interface; include mysql2 dependency

---
+++ src/v1/interfaces/conceptInterface.ts
@@ -0,0 +1,13 @@
+import { InferCreationAttributes, Model } from "sequelize";
+
+interface ConceptInterface extends Model<InferCreationAttributes<ConceptInterface>> {
+    id: number;
+    name: string;
+    type: 'insumo' | 'gasto_operativo';
+    isInventory: boolean;
+    baseUnity: string;
+    purchasingUnit: string | null;
+    conversionUnit: number | null;
+    image: string | null;
+}
+export default ConceptInterface;
